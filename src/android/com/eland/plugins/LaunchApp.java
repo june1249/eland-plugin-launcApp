43fa691f030630df18af3b982b020947b2a4155b
june1249 <kliz78@gmail.com>
2018-08-08 16:50:42 +0900

    plugin.xml 수정

--- src/android/com/eland/plugins/LaunchApp.java
+++ src/android/com/eland/plugins/LaunchApp.java
@@ -10,21 +10,21 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 
 public class LaunchApp extends CordovaPlugin {
-	
+
 	private String LAUNCH_APP = "launch_app";
 
 	@Override
 	public boolean execute(String action, JSONArray args,
 			CallbackContext callbackContext) throws JSONException {
-		
-		if(action.equals(LAUNCH_APP)) {			
+
+		if(action.equals(LAUNCH_APP)) {
 			JSONObject arg_object = args.getJSONObject(0);
 			String launchPackageName = arg_object.getString("packageName");
-			
+
 			PackageManager pm = cordova.getActivity().getPackageManager();
 			Intent intent = pm.getLaunchIntentForPackage(launchPackageName);
 			cordova.getActivity().startActivity(intent);
-		}		
+		}
 		return super.execute(action, args, callbackContext);
 	}
 }
